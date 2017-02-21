c6b55aaab3f7c61fdf011e42b44952d2d00a1d8b
unknown <szymonpajdo@gmail.com>
2017-02-21 19:47:57 +0100

    Developing lambda skills

--- src/sda/finances/Application.java
+++ src/sda/finances/Application.java
@@ -4,9 +4,11 @@ package sda.finances;
 import sda.finances.model.Expense;
 import sda.finances.model.Product;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Application {
 
@@ -18,39 +20,101 @@ public class Application {
         expenses.forEach(expense -> {
             expense.getProducts().stream()
                     .filter(product -> product.getUnitPrice() <= 3)
-                    .forEach(e2 -> System.out.println(e2));
+                    .forEach(System.out::println);
 
 
         });
+
+
+        double banan = expenses.stream()
+                .mapToDouble(expense -> expense.getProducts()
+                        .stream()
+                        .filter(product -> product.getName().equals("banan"))
+                        .mapToDouble(product -> product.getUnitPrice() * product.getAmount())
+                        .sum()
+
+                )
+                .sum();
+        System.out.println(banan);
+
+
+        expenses.stream()
+
+                .filter(expense ->
+                        expense.getDate().isBefore(LocalDate.of(2017, 2, 19)))
+                .forEach(expense -> expense.getProducts()
+                        .forEach(System.out::println));
+
+
+        expenses.forEach(expense -> expense.getProducts()
+                .stream()
+                .filter(product -> product.getName().startsWith("t"))
+                .forEach(System.out::println));
+
+        System.out.println();
+
+
+        System.out.println(expenses.stream()
+                .mapToDouble(expense -> expense.getProducts().stream()
+
+                        .filter(product -> product.getAmount() % 2 == 0)
+                        .mapToDouble(product -> product.getAmount() * product.getUnitPrice())
+                        .sum())
+                .sum());
+
+        expenses.stream()
+                .map(expense -> expense.getProducts()
+                        .stream()
+                        .max((e1, e2) ->
+                                (e1.getUnitPrice() * e1.getAmount()) >
+                                (e2.getAmount() * e2.getUnitPrice()) ? 1 : -1)
+                        .get())
+                .forEach(System.out::println);
+
+        expenses.stream()
+                .map(expense -> expense.getProducts()
+                        .stream()
+                        .max((e1, e2) ->
+                                (e1.getUnitPrice() * e1.getAmount()) >
+                                (e2.getAmount() * e2.getUnitPrice()) ? 1 : -1)
+                        .get())
+                .max((e1, e2) ->
+
+                        (e1.getUnitPrice() * e1.getAmount()) >
+                                (e2.getAmount() * e2.getUnitPrice()) ? 1 : -1)
+
+                        .get();
+
+
     }
 
-        private static List<Expense>init(){
-            List<Product> products = new ArrayList<>();
-            products.add(new Product("banan", 5, 12));
-            products.add(new Product("baklazan", 8, 5));
-            products.add(new Product("batat", 4, 20));
-            products.add(new Product("twojaStara", 1, 1));
-            Expense expense = new Expense("Spozywcze", products);
+    private static List<Expense> init() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("banan", 5, 12));
+        products.add(new Product("baklazan", 8, 5));
+        products.add(new Product("batat", 4, 20));
+        products.add(new Product("twojaStara", 1, 1));
+        Expense expense = new Expense("Spozywcze", products);
 
-            List<Product> products2 = new ArrayList<>();
-            products.add(new Product("farba", 5, 12));
-            products.add(new Product("mlotek", 3, 3));
-            products.add(new Product("gwozdzie", 1, 20));
-            products.add(new Product("twojStary", 1, 1));
-            Expense expense2 = new Expense("budowlane", products2, 2017, 2, 19);
+        List<Product> products2 = new ArrayList<>();
+        products.add(new Product("farba", 5, 12));
+        products.add(new Product("mlotek", 3, 3));
+        products.add(new Product("gwozdzie", 1, 20));
+        products.add(new Product("twojStary", 1, 1));
+        Expense expense2 = new Expense("budowlane", products2, 2017, 2, 19);
 
-            List<Product> products3 = new ArrayList<>();
-            products.add(new Product("accodin", 25, 3));
-            products.add(new Product("wellbutrin", 3, 10));
-            products.add(new Product("konopie", 100, 10));
-            products.add(new Product("machupacabra", 2, 2));
-            Expense expense3 = new Expense("lekarstwa", products3, 2017, 2, 18);
+        List<Product> products3 = new ArrayList<>();
+        products.add(new Product("accodin", 25, 3));
+        products.add(new Product("wellbutrin", 3, 10));
+        products.add(new Product("konopie", 100, 10));
+        products.add(new Product("machupacabra", 2, 2));
+        Expense expense3 = new Expense("lekarstwa", products3, 2017, 2, 18);
 
 
-            return Arrays.asList(expense, expense2, expense3);
+        return Arrays.asList(expense, expense2, expense3);
 
-        }
     }
+}
 
 
 
